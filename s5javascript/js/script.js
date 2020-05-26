32d89ba84d29bf812bc7647e274651f4d86bedde
codigoVis <etneciv2@icloud.com>
2020-05-26 15:38:53 -0500

    se subio carpetas de Bootstrap y  javaScrips

---
+++ s5javascript/js/script.js
@@ -0,0 +1,23 @@
+var numberOne= 8;
+var numberTwo=6;
+
+var totalSuma = numberOne + numberTwo;
+console.log("El resultado de la suma es ",totalSuma);
+
+var totalResta = numberOne - numberTwo;
+console.log("El resultado  de la resta es",totalResta);
+
+var totalMul = numberOne * numberTwo;
+console.log("El resultado de la multiplicacion es",totalMul);
+
+var totalDiv = numberOne / numberTwo;
+console.log("El resultado de la division es ",totalDiv);
+
+var totalRes = numberOne % numberTwo;
+console.log("El resultado del  Modulo es ",totalRes);
+
+
+
+
+
+
